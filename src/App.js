7bc7c6547f9a9e82d3a50424c976b006da034fdb
Nicolle Koetzler <nickkoetzler@gmail.com>
2024-06-09 23:04:08 -0300

    homepage main section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import joaoImage from "./assets/joaoimage.webp";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Section>
+      <Container>
+        <Left>
+          <h1>Eaí! Me chamo João Silva.</h1>
+          <p>Seja bem vindo ao meu WebFólio!
+          Sou Desenvolvedor FullStack com 2 anos de experiência e viciado em café!</p>
+          <p>Meu Endereço:
+          CEP 89280-460</p>
+          <Button>Veja Mais</Button>
+        </Left>
+        <Right>
+        <img src={joaoImage} alt="Joao Silva" />
+        </Right>
+      </Container>
+    </Section>
   );
 }
+
+const Section = styled.div`
+  background: #f3f3f3;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  max-width: 1200px;
+  display: flex;
+  justify-content: center;
+`;
+
+const Left = styled.div`
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  color: #181717;
+`;
+
+const Right = styled.div`
+`;
+
+const Button = styled.button`
+  background: #E07C20;
+  border: none;
+  padding: 10px 20px;
+  color: white;
+`;
 
 export default App;
